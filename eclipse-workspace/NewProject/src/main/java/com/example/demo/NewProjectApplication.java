6c61dfe5f6575869914e7de7b7865f93b95a4522
CoderNawaki <50882239+CoderNawaki@users.noreply.github.com>
2023-11-16 23:37:43 +0900

    create the commandlinerunner

--- eclipse-workspace/NewProject/src/main/java/com/example/demo/NewProjectApplication.java
+++ eclipse-workspace/NewProject/src/main/java/com/example/demo/NewProjectApplication.java
@@ -9,4 +9,16 @@ public class NewProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NewProjectApplication.class, args);
 	}
+
+
+CommandLineRunner.run()throws Exception{
+
+	System.out.println("printing the data for commandLineRunner");
+
+	for(int i =0; i<10; i++){
+		String data = "sample data" +i;
+		System.out.printlln(data);
+
+	}
+}
 }
