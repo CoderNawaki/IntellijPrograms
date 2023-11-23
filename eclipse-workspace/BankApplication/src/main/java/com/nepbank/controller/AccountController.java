09015d6b72896c75a2fb605a50f908d191648dc9
CoderNawaki <50882239+CoderNawaki@users.noreply.github.com>
2023-11-24 00:11:29 +0900

    update account controller

--- eclipse-workspace/BankApplication/src/main/java/com/nepbank/controller/AccountController.java
+++ eclipse-workspace/BankApplication/src/main/java/com/nepbank/controller/AccountController.java
@@ -12,7 +12,53 @@ public List<Account>getAllAccounts(){
 
 @GetMapping("/{id}")
 public ResponseEntity<Account>getAccountById(@PathVariable Long id){
-    
+    Account account = accountService.getAccountById(id);
+
+    if(account!=null){
+        return ResponseEntity.ok(account);
+
+    }else{
+        return ResponseEntity.notFound().build();
+    }
+
+}
+
+@PostMapping
+public ResponseEntity<String> createAccount(@RequestBody Account account){
+    if(account.getBalance()<0){
+        return ResponseEntity.badRequest().body("Balance must be non-negative");
+
+    }
+
+    accountService.saveAccount(account);
+    return ResponseEntity.status(HttpStatus.CREATED).body("Account created successfully");
+
+}
+
+@PutMapping("/{id}"){
+public ResponseEntity<String>updateAccount(@PathVariable Long id, @RequestBody Account account){
+    Account existingAccount = accountService.getAccountById(id);
+    if(existingAccount!=null){
+        //update account properties as needed.
+        existingAccount.setBalance(account.getBalance());
+        accountService.saveAccount(existingAccount);
+        return ResponseEntity.ok("Account updated successfully");
+    }else{
+        return ResponseEntity.notFound().build();
+    }
+}
+
+@DeleteMapping("/{id}")
+public ResponseEntity<String> deleteAccount(@PathVariable Long id ){
+    Account existingAccount = accountService.getAccountById(id);
+    if(existingAccount!=null){
+        accountService.deleteAccount(id);
+        return ResponseEntity.ok("account deleted successfully");
+
+    }else{
+        return ResponseEntity.notFound().build();
+    }
+}
 }
 
 }
