631d72ab2ddb078f57a41b0cb101bc5fc4865956
CoderNawaki <50882239+CoderNawaki@users.noreply.github.com>
2023-11-23 23:59:02 +0900

    update accountcontroller for some validation check

--- eclipse-workspace/BankApplication/src/main/java/com/nepbank/controller/AccountController.java
+++ eclipse-workspace/BankApplication/src/main/java/com/nepbank/controller/AccountController.java
@@ -1,5 +1,18 @@
 
 
 public class AccountController{
+
+   @Autowired 
+private AccountService accountService;
+
+@GetMapping
+public List<Account>getAllAccounts(){
+    return accountService.getAllAccounts();
+}
+
+@GetMapping("/{id}")
+public ResponseEntity<Account>getAccountById(@PathVariable Long id){
     
+}
+
 }
