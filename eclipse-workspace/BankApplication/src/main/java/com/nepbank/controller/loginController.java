c78c3f73e8ca3b6b19ccb1f9a88dab9cc826478c
CoderNawaki <50882239+CoderNawaki@users.noreply.github.com>
2023-11-25 23:58:57 +0900

    update login controller and login.html for better code

--- eclipse-workspace/BankApplication/src/main/java/com/nepbank/controller/loginController.java
+++ eclipse-workspace/BankApplication/src/main/java/com/nepbank/controller/loginController.java
@@ -3,7 +3,15 @@
 public class loginController{
 
     @GetMapping("/login")
-    public String login(){
+    public String loginPage(@RequestParam(name="error",required=false)String error,@RequestParam(name="logout",required=false)String logout,Model model){
+        
+        if(error!=null){
+            model.addAttribute("error","Invalid username or password");
+        }
+        if(logout!=null){
+            model.addAttribute("message","Logged out successfully");
+        }
+
         return "login";
     }
 }
