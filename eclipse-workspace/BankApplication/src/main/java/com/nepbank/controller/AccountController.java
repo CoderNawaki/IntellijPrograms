3401684d860e1ee122f48d45dc5c21baa5f5da5d
CoderNawaki <50882239+CoderNawaki@users.noreply.github.com>
2023-11-28 00:21:13 +0900

    update notificatoin for deposit withdrawal and transfer of funds.

--- eclipse-workspace/BankApplication/src/main/java/com/nepbank/controller/AccountController.java
+++ eclipse-workspace/BankApplication/src/main/java/com/nepbank/controller/AccountController.java
@@ -5,6 +5,70 @@ public class AccountController{
 @Autowired 
 private AccountService accountService;
 
+@Autowired
+private UserController userController;
+
+
+@PostMapping("/deposit/{accountId}/{amount}")
+public ResponseEntity<String>deposit(@PathVariable Long accountId,@PathVariable double amount){
+    Account account =. accountService.getAccountById(accountId);
+    if(account!=null){
+        account.setBalance(account.getBalance()+amount);
+        accountService.saveAccount(account);
+
+        //notify user about the deposit
+        userController.notifyUser("Deposit of"+amount+" to Account"+account.getAccountNumber()+" completed.");
+
+        return ResponseEntity.ok("Deposit completed.");
+
+    }else{
+        return ResponseEntity.notFound().build();
+    }
+}
+
+@PostMapping("/withdraw/{accountId}/{amount}")
+public ResponseEntity<String> withdraw(@PathVariable Long accountId,@PathVariable double amount){
+    Account account = accountService.getAccountById(accountId);
+    if(account!=null && account.getBalance() >=amount){
+        account.setBalance(account.getBalanc() - amount);
+        accountService.saveAccount(account);
+
+        //notify the user about the withdrawal
+
+        userController.notifyUser("Withdrawal of "+amount + " from Account "+ account.getAccountNumber());
+        return ResponseEntity.ok("Withdrawal completed.");
+
+    }else{
+        return ResponseEntity.badRequest().body("Insufficient funds.");
+    }
+}
+
+@PostMapping("/transfer/{sourceAccountId}/{targetAccountId}/{amount}")
+public ResponseEntity<String> transfer(@PathVariable Long sourceAccountId, @PathVariable Long targetAccountId,@PathVariable double amount){
+    Account sourceAccount = accountService.getAccountById(sourceAccountId);
+    Account targetAccount = accountService.getAccountById(targetAccountId);
+
+    if(sourceAccount!=null && targetAccount!=null && sourceAccount.getBalance()>=amount){
+        sourceAccount.setBalance(sourceAccount.getBalance()-amount);
+        targetAccount.setBalance(targetAccount.getBalance() + amount);
+
+        accountService.saveAccount(sourceAccount);
+        accountService.saveAccount(targetAccount);
+        //notify user about the transfer
+
+        userController.notifyUser("Transfer of "+amount+ "from Account "+ sourceAccount.getAccountNumber()+
+        " to Account" + targetAccount.getAccountNumber()+ " completed. ");
+
+        return ResponseEntity.ok("Transfer completed.");
+
+
+    }else{
+        return ResponseEntity.badRequest().body("Invalid transfer or insufficient funds.");
+    }
+} 
+
+
+    
 @GetMapping
 public ResponseEntity<List<Account>>getAllAccounts(){
     List<Account> accounts =accountService.getAllAccounts();
