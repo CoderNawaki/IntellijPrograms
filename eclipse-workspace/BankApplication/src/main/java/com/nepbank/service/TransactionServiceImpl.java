fa0d47bde9ca40961e5812bcd81223fee4232ebd
CoderNawaki <50882239+CoderNawaki@users.noreply.github.com>
2023-11-29 00:05:44 +0900

    updating home html for search feature

--- eclipse-workspace/BankApplication/src/main/java/com/nepbank/service/TransactionServiceImpl.java
+++ eclipse-workspace/BankApplication/src/main/java/com/nepbank/service/TransactionServiceImpl.java
@@ -3,5 +3,15 @@ public class TransactionServiceImpl implements TransactionService{
 
     @Autowired
     private TransactionRepository TransactionRepository;
-    
+
+    @Override
+    public List<Transaction> getAllTransactions(){
+        return TransactionRepository.findAll();
+    }
+
+    @Override
+    public List<Transaction>searchTransactions(String keyword){
+        return TransactionRepository.findByDescriptionContainingIgnoreCase(keyword);
+    }
+
 }
