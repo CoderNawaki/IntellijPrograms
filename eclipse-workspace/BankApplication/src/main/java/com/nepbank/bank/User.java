15e59a5c88bd5752ce9286708b5f410e53e6acbf
CoderNawaki <50882239+CoderNawaki@users.noreply.github.com>
2023-11-30 23:54:59 +0900

    add feature for login with faceId  in first phase.

--- eclipse-workspace/BankApplication/src/main/java/com/nepbank/bank/User.java
+++ eclipse-workspace/BankApplication/src/main/java/com/nepbank/bank/User.java
@@ -7,5 +7,8 @@ public class User{
     @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
 
+    @Column(name="face_id")
+    private String faceId;
+
     // ... (getters and setters)
 }
--- eclipse-workspace/BankApplication/src/main/java/com/nepbank/controller/loginController.java
+++ eclipse-workspace/BankApplication/src/main/java/com/nepbank/controller/loginController.java
@@ -2,6 +2,11 @@
 @Controller
 public class loginController{
 
+    @Autowired
+    private UserService userService;
+
+ 
+
     @GetMapping("/login")
     public String loginPage(@RequestParam(name="error",required=false)String error,@RequestParam(name="logout",required=false)String logout,Model model){
         
@@ -13,5 +18,35 @@ public class loginController{
         }
 
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam(name="username")String username,
+                        @RequestParam(name="password")String password,
+                        @RequestParam(name="faceId",required=false)MultipartFile faceIdImage,
+                        Model model){
+      //authenticate using username and password
+      UserDetails userDetails = userService.loadUserByUsername(username);
+      if(userDetails!=null && userDetails.getPassword().equals(password)){
+        //check face id if provided
+        if(faceIdImage!=null && verifyFaceId(userDetails.getUsername(),faceIdImage)){
+            return "redirect:/home";
+        }else{
+            model.addAttribute("error",Face ID verification failed);
+
+        }
+      }else{
+        model.addAttribute("error","Invalid username or password");
+
+      }  
+      return "login";                    
+
+    }
+
+    private boolean verifyFaceId(String username, MultipartFile faceIdImage){
+        //implement face id verification logic here
+        //you may use a facial recognition library or api
+        //return true if the face id is verified,false otherwise
+        return false;
     }
 }
